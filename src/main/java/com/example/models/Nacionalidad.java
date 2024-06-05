5f0fbec35f5fa9a91efe382666d92d2f88d1c0fd
Benjamin San Martin <b.sanmartin06@ufromail.cl>
2024-06-05 16:21:32 -0400

    Proyecto actualizado

--- src/main/java/com/example/models/Nacionalidad.java
+++ src/main/java/com/example/models/Nacionalidad.java
@@ -1,21 +1,63 @@
 package com.example.models;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+
+enum nacionalidad {
+    Alemania,
+    Argentina,
+    Australia,
+    Austria,
+    Belgica,
+    Bolivia,
+    Brasil,
+    Canada,
+    Chile,
+    China,
+    Colombia,
+    Corea_del_Sur,
+    Costa_Rica,
+    Croacia,
+    Cuba,
+    Dinamarca,
+    Ecuador,
+    Egipto,
+    El_Salvador,
+    Emiratos_Arabes_Unidos,
+    Escocia,
+    Eslovaquia,
+    Eslovenia,
+    Espana,
+    Estados_Unidos,
+    Estonia,
+    Filipinas,
+    Finlandia,
+    Francia,
+    Grecia,
+    Guatemala,
+    Holanda,
+    Honduras,
+    Hungria,
+    India,
+    Indonesia,
+    Inglaterra,
+    Irak,
+    Iran,
+    Irlanda,
+    Irlanda_del_Norte,
+    Islandia,
+}
 
 @Entity
+@Data
+
 public class Nacionalidad {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idNacional;
+    private Long id_nacionalidad;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private String nombre;
 }
